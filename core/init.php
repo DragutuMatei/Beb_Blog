808379d349d7ae52fd9b1a3ed4500901f7897f03
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-03 21:55:23 +0300

    make it better

--- core/init.php
+++ core/init.php
@@ -1,12 +1,24 @@
 <?php
 session_start();
 
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+
 $GLOBALS['config'] = array(
     'mysql' => array(
-        'host' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'db' => 'id17602979_bebe'
+        'host' => $cleardb_server,
+        'username' => $cleardb_username,
+        'password' => $cleardb_password,
+        'db' => $cleardb_db
+        
+        // 'host' => "eu-cdbr-west-01.cleardb.com",
+        // 'username' => "b8f4cd3c10b0c1",
+        // 'password' => "6f3c670c",
+        // 'db' => "heroku_a9f91118cf15b13"
     ),
     "remember" => array(
         'cookie_name' => 'hash',
