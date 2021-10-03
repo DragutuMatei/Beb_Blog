179011da4acc518bcf059469726d13d20daae4e8
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-03 23:08:32 +0300

    make it better

--- admini.php
+++ admini.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "./core/init.php";
+require_once "./core/init.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,91 +25,92 @@
 
 <body>
 
-<br><br><br><br>
 
-        <?php
-        if(!isset($_SESSION['admin'])){
-            Redirect::to("admin.php");
-        }
-        ?>
 
-        <form method="POST" enctype="multipart/form-data" >
+    <?php
+    require_once './components/navbar.php';
+    echo "<br><br><br><br><br><br><br><br>";
+    if (!isset($_SESSION['admin'])) {
+        Redirect::to("admin.php");
+    }
+    ?>
+
+    <form method="POST" enctype="multipart/form-data">
         <?php
-            if (Input::exists()) {
-                // if (Token::check(Input::get("token"))) {
-                    $validate = new Validation();
-    
-                    $validate = $validate->check($_POST, array(
-                        "titlu" => array(
-                            "min" => 5,
-                            "required" => true
-                        ),
-                        "text" => array(
-                            "min" => 6,
-                            "required" => true,
-                        ),
-                    ));
-    
-    
-                    if ($validate->passed()) {
-                        $files = array_filter($_FILES["imagini"]["name"]);
-                        $cate_is = count($_FILES["imagini"]["name"]);
-    
-                        $array_cu_imag = array();
-    
-                       for ($i = 0; $i < $cate_is; $i++) {
-                            $temporale = $_FILES["imagini"]["tmp_name"][$i];
-    
-                            $imageFileType = strtolower(pathinfo($temporale, PATHINFO_EXTENSION));
-    
-                            // if ($temporale["size"] >= 2000000) {
-                                // if (
-                                    // $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                                    // && $imageFileType != "gif" && $imageFileType != "svg"
-                                // ) {
-                                    // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                                // } else 
-                                if ($temporale != "") {
-                                    $array_cu_imag[$i] = "./assets/img/" . $_FILES["imagini"]["name"][$i];
-                                    $newFilePath = "./assets/img/" . $_FILES["imagini"]["name"][$i];
-                                    move_uploaded_file($temporale, $newFilePath);
-                                } 
-                        }
-    
-                        $imgs = array();
-                        for ($i = 0; $i < count($array_cu_imag); $i++) {
-                            $imgs[$i] = $array_cu_imag[$i];
-                        }
-    
-                        $add = new AddBlogPost();
-    
-                        try {
-                            $add->add(array(
-                                "titlu" => Input::get("titlu"),
-                                "text" => Input::get("text"),
-                                "poze" => json_encode($imgs),
-                                "likes" => 0
-                            ));
-                            
-                            Redirect::to("admini.php");
-                            
-                        } catch (Exception $e) {
-                            die($e->getMessage());
-                        }
-                    } else {
-                        foreach ($validate->errors() as $error) {
-                            echo "<h2>" . $error . "</h2><br/>";
-                        }
+        if (Input::exists()) {
+            // if (Token::check(Input::get("token"))) {
+            $validate = new Validation();
+
+            $validate = $validate->check($_POST, array(
+                "titlu" => array(
+                    "min" => 5,
+                    "required" => true
+                ),
+                "text" => array(
+                    "min" => 6,
+                    "required" => true,
+                ),
+            ));
+
+
+            if ($validate->passed()) {
+                $files = array_filter($_FILES["imagini"]["name"]);
+                $cate_is = count($_FILES["imagini"]["name"]);
+
+                $array_cu_imag = array();
+
+                for ($i = 0; $i < $cate_is; $i++) {
+                    $temporale = $_FILES["imagini"]["tmp_name"][$i];
+
+                    $imageFileType = strtolower(pathinfo($temporale, PATHINFO_EXTENSION));
+
+                    // if ($temporale["size"] >= 2000000) {
+                    // if (
+                    // $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                    // && $imageFileType != "gif" && $imageFileType != "svg"
+                    // ) {
+                    // echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                    // } else 
+                    if ($temporale != "") {
+                        $array_cu_imag[$i] = "./assets/img/" . $_FILES["imagini"]["name"][$i];
+                        $newFilePath = "./assets/img/" . $_FILES["imagini"]["name"][$i];
+                        move_uploaded_file($temporale, $newFilePath);
                     }
-                // } else{
-                //     echo "debug1<br>";
-                // }
+                }
+
+                $imgs = array();
+                for ($i = 0; $i < count($array_cu_imag); $i++) {
+                    $imgs[$i] = $array_cu_imag[$i];
+                }
+
+                $add = new AddBlogPost();
+
+                try {
+                    $add->add(array(
+                        "titlu" => Input::get("titlu"),
+                        "text" => Input::get("text"),
+                        "poze" => json_encode($imgs),
+                        "likes" => 0
+                    ));
+
+                    Redirect::to("admini.php");
+                } catch (Exception $e) {
+                    die($e->getMessage());
+                }
+            } else {
+                foreach ($validate->errors() as $error) {
+                    echo "<h2>" . $error . "</h2><br/>";
+                }
             }
-            // else{
-            //     echo "<br>debug2";
+            // } else{
+            //     echo "debug1<br>";
             // }
+        }
+        // else{
+        //     echo "<br>debug2";
+        // }
         ?>
-    
+
         <div class="mb-3">
             <label for="tit" class="form-label">Scrie titlul poeziei fara semnul intrebarii sau emoticoane</label>
 
@@ -135,15 +136,15 @@
     <section class="work section" id="work" style="margin-top: 40px;">
 
         <div class="work__container bd-grid">
-            
+
             <?php
-                require_once "./components/get.php";
+            require_once "./components/get.php";
             ?>
         </div>
     </section>
-    
-   
-    
+
+
+
 </body>
 
 </html>
