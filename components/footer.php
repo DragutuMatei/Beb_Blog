ecdd52f119711701cad73bdb3a2416ab10229788
DragutuMatei <dragutumatei573@yahoo.ro>
2021-10-03 22:59:33 +0300

    make it better

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <p class="footer__title">Sevastre Beatrice</p>
     <div class="footer__social">
-        <a href="#" class="footer__icon"><i class='bx bxl-facebook'></i></a>
-        <a href="#" class="footer__icon"><i class='bx bxl-instagram'></i></a>
-        <a href="#" class="footer__icon"><i class='bx bxl-twitter'></i></a>
+        <a href="https://www.facebook.com/justbebe7484" class="footer__icon"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/beatricesevastre/" class="footer__icon"><i class="fab fa-instagram"></i></a>
+        <a href="mailto:sevastrebeatrice@gmail.com" class="footer__icon"> <i class="fas fa-envelope-open"></i></i></a>
     </div>
     <p>&#169; 2020 copyright all right reserved</p>
     <!-- site facut de Dragutu Matei 
